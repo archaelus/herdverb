340da56cb205be15912f9ab34c69b248cde0426e
Geoff Cant <nem@erlang.geek.nz>
2012-11-24 00:11:14 +1300

    More cowboy 2.0 fixups.

--- src/herdverb_http_handler.erl
+++ src/herdverb_http_handler.erl
@@ -14,13 +14,13 @@ init({_Any, http}, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {Method, Req2} = cowboy_http_req:method(Req),
+    {Method, Req2} = cowboy_req:method(Req),
     ?INFO("at=request method='~s'", [Method]),
     Body = io_lib:format("at=request method='~s'", [Method]),
-    {ok, Req3} = cowboy_http_req:reply(200,
-                                       [{'Content-Type', "text/plain"}],
-                                       Body,
-                                       Req2),
+    {ok, Req3} = cowboy_req:reply(200,
+                                  [{<<"Content-Type">>, <<"text/plain">>}],
+                                  Body,
+                                  Req2),
     {ok, Req3, State}.
 
 terminate(_Req, _State) ->
