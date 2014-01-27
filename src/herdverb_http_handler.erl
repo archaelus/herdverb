0e5b35dd0fdaaee62650b830b1175c4ef88a3f64
Geoff Cant <nem@erlang.geek.nz>
2014-01-27 14:47:15 -0800

    Latest http handler improvements.

--- src/herdverb_http_handler.erl
+++ src/herdverb_http_handler.erl
@@ -10,39 +10,41 @@
 
 -include("herdverb_log.hrl").
 
+-define(CRLF, <<"\r\n">>).
+
+
 init({_Any, http}, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {Method, Req2} = cowboy_req:method(Req),
-    ?INFO("at=request method='~s'", [Method]),
-    {Body, Req3} = format(Req2),
-    {ok, Req4} = cowboy_req:reply(200,
+    {ReqIOList, Req2} = to_iolist(Req),
+    ?INFO("Request: ~s", [ReqIOList]),
+    {ok, Req3} = cowboy_req:reply(200,
                                   [{<<"Content-Type">>, <<"text/plain">>}],
-                                  Body,
-                                  Req3),
-    {ok, Req4, State}.
+                                  ReqIOList,
+                                  Req2),
+    {ok, Req3, State}.
 
 terminate(_Req, _Reason, _State) ->
     ok.
 
-format(Req) ->
-    {{Fmt, Args}, Req2} = serialize(Req),
-    {io_lib:format(Fmt, Args), Req2}.
-
-serialize(Req) ->
-    {[Method, Path, Version, Headers], Req2} =
-        lists:foldr(fun (F, {Acc, ReqM}) ->
-                            {V, ReqM1} = cowboy_req:F(ReqM),
-                            {[V | Acc], ReqM1}
-                    end,
-                    {[], Req},
-                    [method, path, version, headers]),
-    {ok, Body, Req3} = cowboy_req:body(Req2),
-    {{lists:flatten(["~s ~s ~s~n",
-                     ["~s: ~s~n"
-                      || _ <- Headers ],
-                     "~n~s"]),
-      [Method, Path, atom_to_list(Version) |
-       lists:append([[K, V] || {K, V} <- Headers]) ++ [Body]]},
+to_iolist(Req) ->
+    {Status, Req2} = status_to_iolist(Req),
+    {Headers, Req3} = headers_to_iolist(Req2),
+    {[Status, ?CRLF,
+      Headers,
+      ?CRLF],
      Req3}.
+
+status_to_iolist(Req) ->
+    {Method, Req2} = cowboy_req:method(Req),
+    {Path, Req3} = cowboy_req:path(Req2),
+    {Version, Req4} = cowboy_req:version(Req3),
+    {[Method, " ", Path, " ", atom_to_binary(Version, latin1)],
+     Req4}.
+
+headers_to_iolist(Req) ->
+    {Headers, Req2} = cowboy_req:headers(Req),
+    {[ [K, <<": ">>, V, ?CRLF]
+       || {K, V} <- Headers ],
+     Req2}.
